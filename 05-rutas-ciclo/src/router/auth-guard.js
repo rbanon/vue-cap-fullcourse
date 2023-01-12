55a6346355037d9da3db215a52b2c13d47b7511f
Rafael Bañón <rafael.banon-martin@capgemini.com>
2023-01-12 11:51:05 +0100

    Auth-guard

--- 05-rutas-ciclo/src/router/auth-guard.js
+++ 05-rutas-ciclo/src/router/auth-guard.js
@@ -1,4 +1,16 @@
-const isAuthenticatedGuard = (to,from, next) => {
+const isAuthenticatedGuard = async (to,from, next) => {
+    return new Promise(() => {
+
+        const random = Math.random()*100
+        if (random > 50){
+            console.log("Autenticado")
+            next()
+        } else {
+            console.log("Blqueado por isAuthenticatedGuard", random)
+            next({name:'pokemon-home'})
+        }
+
+    })
     console.log({to,from,next})
 
 }
