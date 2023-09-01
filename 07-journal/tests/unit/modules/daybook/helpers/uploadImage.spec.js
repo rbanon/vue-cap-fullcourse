6123abdb6873e6425a33c7b056100c04886c6b29
Rafael Bañón <rafael.banon-martin@capgemini.com>
2023-09-01 08:36:57 +0200

    UploadImage test w/errors(203)

--- 07-journal/tests/unit/modules/daybook/helpers/uploadImage.spec.js
+++ 07-journal/tests/unit/modules/daybook/helpers/uploadImage.spec.js
@@ -10,11 +10,26 @@ describe("Pruebas en el helper uploadImage", () => {
         responseType: "arraybuffer",
       }
     );
-
     const file = new File([data], "foto.jpg");
-
     const url = await uploadImage(file);
 
     expect(typeof url).toBe("string");
   });
+
+  // it("Debe de cargar un archivo y retornar el URL", () => {
+  //   axios
+  //     .get(
+  //       "https://res.cloudinary.com/dvhy9z3n4/image/upload/v1674815473/w0y5x6raaoy8akpiagqr.jpg",
+  //       {
+  //         responseType: "arraybuffer",
+  //       }
+  //     )
+  //     .then((response) => {
+  //       const file = new File([response.data], "foto.jpg");
+  //       uploadImage(file).then((response) => {
+  //         console.log(response);
+  //         expect(typeof url).toBe("string");
+  //       });
+  //     });
+  // });
 });
